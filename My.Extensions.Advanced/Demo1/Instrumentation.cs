a1d3c7dacbcca738d80e79ab2f21fe469c961829
skhandorin <hsatig@mail.ru>
2017-03-20 19:28:51 +0500

    Advanced Extensions Methods - Demo2 - beginning

---
+++ My.Extensions.Advanced/Demo1/Instrumentation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My.Extensions.Advanced.Demo1
+{
+    public sealed class Instrumentation
+    {
+        public Guid Id { get; private set; }
+
+        private DateTime startedAt;
+
+        public string ProcessName { get; set; }
+
+        public Instrumentation()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public void Start()
+        {
+            startedAt = DateTime.Now;
+        }
+
+        public int GetElapsedTime()
+        {
+            return (int)Math.Round(new TimeSpan(DateTime.Now.Ticks - startedAt.Ticks).TotalSeconds, 0);
+        }
+    }
+}
