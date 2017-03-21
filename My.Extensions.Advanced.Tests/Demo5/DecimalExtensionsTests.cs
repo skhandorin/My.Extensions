883c3f3547c7b427f2ee2f51d177194ef79d5107
skhandorin <hsatig@mail.ru>
2017-03-21 15:47:23 +0500

    Module 3 completetd

---
+++ My.Extensions.Advanced.Tests/Demo5/DecimalExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using My.Extensions.Advanced.Demo5;
+
+namespace My.Extensions.Advanced.Tests.Demo5
+{
+    [TestClass]
+    public class DecimalExtensionsTests
+    {
+        [TestMethod]
+        new public void ToString()
+        {
+            var input = 10.51M;
+            Assert.AreEqual("10,51", input.ToString());
+        }
+
+        [TestMethod]
+        public void ToStringRounded()
+        {
+            var input = 10.51M;
+            Assert.AreEqual("10,5", input.ToStringRounded());
+        }
+    }
+}
---
+++ My.Extensions.Advanced/Demo5/DecimalExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My.Extensions.Advanced.Demo5
+{
+    public static class DecimalExtensions
+    {
+        public static string ToString(this decimal input)
+        {
+            return Math.Round(input, 1).ToString();
+        }
+        
+        public static string ToStringRounded(this decimal input)
+        {
+            return Math.Round(input, 1).ToString();
+        }
+        
+        public static string ToStringRounded(this IComparable input)
+        {
+            return "called IComparable extensions";
+        }
+
+        public static string ToStringRounded(this IComparable<decimal> input)
+        {
+            return "called IComparable<decimal> extensions";
+        }
+    }
+}
